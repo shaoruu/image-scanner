29b6fd9fc47ef42be12c09abaffad986c69ddbee
ian13456 <21ianh1@students.tas.tw>
2020-09-07 21:28:27 +0800

    Updates

---
+++ precache-manifest.ef826c731671da2c78e31c50fc7dcbb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "935c9c4f6007bffa5715b7bbfd81b1b9",
+    "url": "/image-scanner/index.html"
+  },
+  {
+    "revision": "bbc63c0a8880f13e1047",
+    "url": "/image-scanner/static/css/main.5123df8a.chunk.css"
+  },
+  {
+    "revision": "4a98ae974d7828b83a4f",
+    "url": "/image-scanner/static/js/2.b153784a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/image-scanner/static/js/2.b153784a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbc63c0a8880f13e1047",
+    "url": "/image-scanner/static/js/main.07e00d4d.chunk.js"
+  },
+  {
+    "revision": "6905073273ee5b07f294",
+    "url": "/image-scanner/static/js/runtime-main.9745b41a.js"
+  }
+]);
